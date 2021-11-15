a78979cd86ea19dc1e277cb652e71179fc4121ff
Liheng Jian <jianliheng@sensetime.com>
2021-11-15 16:01:16 +0800

    [feature][cuda]add mean().

--- src/ppl/cv/cuda/mean_unittest.cpp
+++ src/ppl/cv/cuda/mean_unittest.cpp
@@ -14,7 +14,7 @@
  * under the License.
  */
 
-#include "ppl/cv/cuda/calchist.h"
+#include "ppl/cv/cuda/mean.h"
 
 #include <tuple>
 #include <sstream>
@@ -32,8 +32,8 @@ enum MaskType {
   MASKED,
 };
 
-using Parameters = std::tuple<MaskType, cv::Size>;
-inline std::string convertToStringHist(const Parameters& parameters) {
+using Parameters = std::tuple<MaskType, bool, cv::Size>;
+inline std::string convertToStringMean(const Parameters& parameters) {
   std::ostringstream formatted;
 
   MaskType is_masked = std::get<0>(parameters);
@@ -44,7 +44,15 @@ inline std::string convertToStringHist(const Parameters& parameters) {
     formatted << "Masked" << "_";
   }
 
-  cv::Size size = std::get<1>(parameters);
+  bool channel_wise = std::get<1>(parameters);
+  if (channel_wise) {
+    formatted << "channel_wise" << "_";
+  }
+  else {
+    formatted << "unchannel_wise" << "_";
+  }
+
+  cv::Size size = std::get<2>(parameters);
   formatted << size.width << "x";
   formatted << size.height;
 
@@ -52,45 +60,47 @@ inline std::string convertToStringHist(const Parameters& parameters) {
 }
 
 template <typename T, int channels>
-class PplCvCudaCalcHistTest : public ::testing::TestWithParam<Parameters> {
+class PplCvCudaMeanTest : public ::testing::TestWithParam<Parameters> {
  public:
-  PplCvCudaCalcHistTest() {
+  PplCvCudaMeanTest() {
     const Parameters& parameters = GetParam();
-    is_masked = std::get<0>(parameters);
-    size      = std::get<1>(parameters);
+    is_masked    = std::get<0>(parameters);
+    channel_wise = std::get<1>(parameters);
+    size         = std::get<2>(parameters);
   }
 
-  ~PplCvCudaCalcHistTest() {
+  ~PplCvCudaMeanTest() {
   }
 
   bool apply();
 
  private:
   MaskType is_masked;
+  bool channel_wise;
   cv::Size size;
 };
 
 template <typename T, int channels>
-bool PplCvCudaCalcHistTest<T, channels>::apply() {
-  cv::Mat src, dst, cv_dst, cv_dst1, mask0;
+bool PplCvCudaMeanTest<T, channels>::apply() {
+  cv::Mat src, mask0;
   src = createSourceImage(size.height, size.width,
                           CV_MAKETYPE(cv::DataType<T>::depth, channels));
-  dst = cv::Mat::zeros(1, 256, CV_MAKETYPE(cv::DataType<int>::depth, 1));
-  cv_dst = cv::Mat::zeros(1, 256, CV_MAKETYPE(cv::DataType<int>::depth, 1));
   mask0 = createSourceImage(size.height, size.width,
                             CV_MAKETYPE(cv::DataType<uchar>::depth, 1));
   cv::cuda::GpuMat gpu_src(src);
-  cv::cuda::GpuMat gpu_dst(dst);
   cv::cuda::GpuMat gpu_mask0(mask0);
+  int dst_size = channels * sizeof(float);
+  float* dst = (float*)malloc(dst_size);
+  float* gpu_dst;
+  cudaMalloc((void**)&gpu_dst, dst_size);
 
   int src_size = size.height * size.width * channels * sizeof(T);
-  int dst_size = 256 * sizeof(int);
   int mask_size = size.height * size.width * sizeof(uchar);
   T* input = (T*)malloc(src_size);
-  int* output = (int*)malloc(dst_size);
+  float* output = (float*)malloc(dst_size);
   uchar* mask1 = (uchar*)malloc(mask_size);
   T* gpu_input;
-  int* gpu_output;
+  float* gpu_output;
   uchar* gpu_mask1;
   cudaMalloc((void**)&gpu_input, src_size);
   cudaMalloc((void**)&gpu_output, dst_size);
@@ -100,71 +110,81 @@ bool PplCvCudaCalcHistTest<T, channels>::apply() {
   cudaMemcpy(gpu_input, input, src_size, cudaMemcpyHostToDevice);
   cudaMemcpy(gpu_mask1, mask1, mask_size, cudaMemcpyHostToDevice);
 
-  int channel[] = {0};
-  int hist_size[] = {256};
-  float data_range[2] = {0, 256};
-  const float* ranges[1] = {data_range};
-
+  cv::Scalar mean_value;
   if (is_masked == UNMASKED) {
-    cv::calcHist(&src, 1, channel, cv::Mat(), cv_dst1, 1, hist_size, ranges,
-                 true, false);
-    CalcHist<T>(0, gpu_src.rows, gpu_src.cols, gpu_src.step / sizeof(T),
-                (T*)gpu_src.data, (int*)gpu_dst.data);
-    gpu_dst.download(dst);
+    mean_value = cv::mean(src);
+    // Mean<T, channels>(0, gpu_src.rows, gpu_src.cols, gpu_src.step / sizeof(T),
+    //                   (T*)gpu_src.data, gpu_dst, 0, nullptr, channel_wise);
+    // cudaMemcpy(dst, gpu_dst, dst_size, cudaMemcpyDeviceToHost);
 
-    CalcHist<T>(0, size.height, size.width, size.width * channels,
-                gpu_input, gpu_output);
+    Mean<T, channels>(0, size.height, size.width, size.width * channels,
+                      gpu_input, gpu_output, 0, nullptr, channel_wise);
     cudaMemcpy(output, gpu_output, dst_size, cudaMemcpyDeviceToHost);
   }
   else {
-    cv::calcHist(&src, 1, channel, mask0, cv_dst1, 1, hist_size, ranges, true,
-                 false);
-    CalcHist<T>(0, gpu_src.rows, gpu_src.cols, gpu_src.step / sizeof(T),
-                (T*)gpu_src.data, (int*)gpu_dst.data,
-                gpu_mask0.step / sizeof(uchar), (uchar*)gpu_mask0.data);
-    gpu_dst.download(dst);
+    mean_value = cv::mean(src, mask0);
+    // Mean<T, channels>(0, gpu_src.rows, gpu_src.cols, gpu_src.step / sizeof(T),
+    //                   (T*)gpu_src.data, gpu_dst, gpu_mask0.step / sizeof(uchar),
+    //                   (uchar*)gpu_mask0.data, channel_wise);
+    // cudaMemcpy(dst, gpu_dst, dst_size, cudaMemcpyDeviceToHost);
 
-    CalcHist<T>(0, size.height, size.width, size.width * channels,
-                gpu_input, gpu_output, size.width, gpu_mask1);
+    Mean<T, channels>(0, size.height, size.width, size.width * channels,
+                      gpu_input, gpu_output, size.width, gpu_mask1,
+                      channel_wise);
     cudaMemcpy(output, gpu_output, dst_size, cudaMemcpyDeviceToHost);
   }
-  cv::Mat temp_mat;
-  cv_dst1.convertTo(temp_mat, CV_32S);
-  cv_dst = temp_mat.reshape(0, 1);
 
   float epsilon = EPSILON_1F;
-  bool identity0 = checkMatricesIdentity<int>(cv_dst, dst, epsilon);
-  bool identity1 = checkMatArrayIdentity<int>(cv_dst, output, epsilon);
+  bool identity = true;
+  if (channel_wise) {
+    for (int i = 0; i < channels; i++) {
+      if (fabs(mean_value.val[i] - output[i]) > epsilon) {
+        identity = false;
+      }
+    }
+  }
+  else {
+    if (fabs(mean_value.val[0] - output[0]) > epsilon) {
+      identity = false;
+    }
+  }
 
+  free(dst);
   free(input);
   free(output);
   free(mask1);
   cudaFree(gpu_input);
+  cudaFree(gpu_dst);
   cudaFree(gpu_output);
   cudaFree(gpu_mask1);
 
-  return (identity0 && identity1);
+  return identity;
 }
 
 #define UNITTEST(T, channels)                                                  \
-using PplCvCudaCalcHistTest ## T ## channels =                                 \
-        PplCvCudaCalcHistTest<T, channels>;                                    \
-TEST_P(PplCvCudaCalcHistTest ## T ## channels, Standard) {                     \
+using PplCvCudaMeanTest ## T ## channels =  PplCvCudaMeanTest<T, channels>;    \
+TEST_P(PplCvCudaMeanTest ## T ## channels, Standard) {                         \
   bool identity = this->apply();                                               \
   EXPECT_TRUE(identity);                                                       \
 }                                                                              \
                                                                                \
-INSTANTIATE_TEST_CASE_P(IsEqual, PplCvCudaCalcHistTest ## T ## channels,       \
+INSTANTIATE_TEST_CASE_P(IsEqual, PplCvCudaMeanTest ## T ## channels,           \
   ::testing::Combine(                                                          \
     ::testing::Values(UNMASKED, MASKED),                                       \
+    ::testing::Values(true, false),                                            \
     ::testing::Values(cv::Size{321, 240}, cv::Size{642, 480},                  \
                       cv::Size{1283, 720}, cv::Size{1934, 1080},               \
                       cv::Size{320, 240}, cv::Size{640, 480},                  \
                       cv::Size{1280, 720}, cv::Size{1920, 1080})),             \
   [](const testing::TestParamInfo<                                             \
-      PplCvCudaCalcHistTest ## T ## channels::ParamType>& info) {              \
-    return convertToStringHist(info.param);                                    \
+      PplCvCudaMeanTest ## T ## channels::ParamType>& info) {                  \
+    return convertToStringMean(info.param);                                    \
   }                                                                            \
 );
 
 UNITTEST(uchar, 1)
+UNITTEST(uchar, 3)
+UNITTEST(uchar, 4)
+UNITTEST(float, 1)
+UNITTEST(float, 3)
+UNITTEST(float, 4)
