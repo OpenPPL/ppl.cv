e1a617ade747033c4ec6fb5c07e2c0d73c7c9ed0
ouguoyu <ouguoyu@sensetime.com>
2022-01-24 15:34:51 +0800

    [doc] how to build with vs2015/vs2019

---
+++ build.bat
@@ -0,0 +1,6 @@
+md pplcv-build
+cd pplcv-build
+cmake %* -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=install ..
+cmake --build . -j --config Release
+cmake --build . --target install -j --config Release
+cd ..
