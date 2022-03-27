095f8817cfea899c99c041972a08bb5218771aea
ChenXin <xinchen.tju@gmail.com>
2022-03-28 01:05:42 +0800

    parallel build for target leval

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 md pplcv-build
 cd pplcv-build
 cmake %* -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=install ..
-cmake --build . --config Release
+cmake --build . --config Release -- /m
 cmake --install . --config Release
 cd ..
