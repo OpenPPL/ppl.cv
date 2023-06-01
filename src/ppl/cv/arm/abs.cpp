b6501e51647ab4189e47a34ad67ab90bd084c0b1
VOIDMalkuth <8085528+VOIDMalkuth@users.noreply.github.com>
2023-06-01 08:55:16 +0000

    [perf][arm] optimize abs op with neon intrinsics

--- src/ppl/cv/arm/abs.cpp
+++ src/ppl/cv/arm/abs.cpp
@@ -15,43 +15,26 @@
 // specific language governing permissions and limitations
 // under the License.
 
-#include "ppl/cv/types.h"
 #include "ppl/cv/arm/abs.h"
+#include "ppl/cv/types.h"
+#include "ppl/common/sys.h"
+#include "common.hpp"
 #include <arm_neon.h>
 #include <algorithm>
+#include <cmath>
 
 namespace ppl {
 namespace cv {
 namespace arm {
 
-template <typename T>
-void abs(T *dst, const T *src, int32_t n)
-{
-    for (int32_t i = 0; i < n; ++i) {
-        if (src[i] == -128) {
-            dst[i] = 127;
-        } else {
-            dst[i] = src[i] < 0 ? -(src[i]) : src[i];
-        }
-    }
-}
-
-template <>
-void abs(float *dst, const float *src, int32_t n)
-{
-    for (int32_t i = 0; i < n; ++i) {
-        dst[i] = src[i] < 0 ? -(src[i]) : src[i];
-    }
-}
-
-template <typename T, int32_t nc>
-::ppl::common::RetCode Abs(
+::ppl::common::RetCode abs_s8(
     int32_t height,
     int32_t width,
+    int32_t channels,
     int32_t inWidthStride,
-    const T *inData,
+    const int8_t *inData,
     int32_t outWidthStride,
-    T *outData)
+    int8_t *outData)
 {
     if (nullptr == inData || nullptr == outData) {
         return ppl::common::RC_INVALID_VALUE;
@@ -59,19 +42,145 @@ template <typename T, int32_t nc>
     if (width == 0 || height == 0 || inWidthStride == 0 || outWidthStride == 0) {
         return ppl::common::RC_INVALID_VALUE;
     }
-    for (int32_t h = 0; h < height; h++) {
-        abs<T>(outData + h * outWidthStride, inData + h * inWidthStride, nc * width);
+    width *= channels;
+    for (int32_t i = 0; i < height; ++i) {
+        int32_t j = 0;
+        const int8_t *in0 = inData + i * inWidthStride;
+        int8_t *out = outData + i * outWidthStride;
+        for (; j <= width - 32; j += 32) {
+            prefetch(in0 + j);
+            int8x16_t vdata0 = vld1q_s8(in0 + j);
+            int8x16_t vdata1 = vld1q_s8(in0 + j + 16);
+            int8x16_t voutData0 = vqabsq_s8(vdata0);
+            int8x16_t voutData1 = vqabsq_s8(vdata1);
+            vst1q_s8(out + j, voutData0);
+            vst1q_s8(out + j + 16, voutData1);
+        }
+        for (; j <= width - 16; j += 16) {
+            int8x16_t vdata0 = vld1q_s8(in0 + j);
+            int8x16_t voutData = vqabsq_s8(vdata0);
+            vst1q_s8(out + j, voutData);
+        }
+        for (; j < width; ++j) {
+            int8_t srcVal = inData[i * inWidthStride + j];
+            outData[i * outWidthStride + j] = (srcVal == -128) ? 127 : std::abs(srcVal);
+        }
     }
     return ppl::common::RC_SUCCESS;
 }
 
-template ::ppl::common::RetCode Abs<float, 1>(int32_t height, int32_t width, int32_t inWidthStride, const float *inData, int32_t outWidthStride, float *outData);
-template ::ppl::common::RetCode Abs<float, 3>(int32_t height, int32_t width, int32_t inWidthStride, const float *inData, int32_t outWidthStride, float *outData);
-template ::ppl::common::RetCode Abs<float, 4>(int32_t height, int32_t width, int32_t inWidthStride, const float *inData, int32_t outWidthStride, float *outData);
+template<>
+::ppl::common::RetCode Abs<int8_t, 1>(
+    int32_t height,
+    int32_t width,
+    int32_t inWidthStride,
+    const int8_t *inData,
+    int32_t outWidthStride,
+    int8_t *outData)
+{
+    return abs_s8(height, width, 1, inWidthStride, inData, outWidthStride, outData);
+}
 
-template ::ppl::common::RetCode Abs<int8_t, 1>(int32_t height, int32_t width, int32_t inWidthStride, const int8_t *inData, int32_t outWidthStride, int8_t *outData);
-template ::ppl::common::RetCode Abs<int8_t, 3>(int32_t height, int32_t width, int32_t inWidthStride, const int8_t *inData, int32_t outWidthStride, int8_t *outData);
-template ::ppl::common::RetCode Abs<int8_t, 4>(int32_t height, int32_t width, int32_t inWidthStride, const int8_t *inData, int32_t outWidthStride, int8_t *outData);
+template<>
+::ppl::common::RetCode Abs<int8_t, 3>(
+    int32_t height,
+    int32_t width,
+    int32_t inWidthStride,
+    const int8_t *inData,
+    int32_t outWidthStride,
+    int8_t *outData)
+{
+    return abs_s8(height, width, 3, inWidthStride, inData, outWidthStride, outData);
+}
+
+template<>
+::ppl::common::RetCode Abs<int8_t, 4>(
+    int32_t height,
+    int32_t width,
+    int32_t inWidthStride,
+    const int8_t *inData,
+    int32_t outWidthStride,
+    int8_t *outData)
+{
+    return abs_s8(height, width, 4, inWidthStride, inData, outWidthStride, outData);
+}
+
+::ppl::common::RetCode abs_f32(
+    int32_t height,
+    int32_t width,
+    int32_t channels,
+    int32_t inWidthStride,
+    const float32_t *inData,
+    int32_t outWidthStride,
+    float32_t *outData)
+{
+    if (nullptr == inData || nullptr == outData) {
+        return ppl::common::RC_INVALID_VALUE;
+    }
+    if (width == 0 || height == 0 || inWidthStride == 0 || outWidthStride == 0) {
+        return ppl::common::RC_INVALID_VALUE;
+    }
+    width *= channels;
+    for (int32_t i = 0; i < height; ++i) {
+        int32_t j = 0;
+        const float *in0 = inData + i * inWidthStride;
+        float *out = outData + i * outWidthStride;
+        for (; j <= width - 8; j += 8) {
+            prefetch(in0 + j);
+            float32x4_t vdata0 = vld1q_f32(in0 + j);
+            float32x4_t vdata1 = vld1q_f32(in0 + j + 4);
+            float32x4_t voutData0 = vabsq_f32(vdata0);
+            float32x4_t voutData1 = vabsq_f32(vdata1);
+            vst1q_f32(out + j, voutData0);
+            vst1q_f32(out + j + 4, voutData1);
+        }
+        for (; j <= width - 4; j += 4) {
+            float32x4_t vdata0 = vld1q_f32(in0 + j);
+            float32x4_t voutData = vabsq_f32(vdata0);
+            vst1q_f32(out + j, voutData);
+        }
+        for (; j < width; ++j) {
+            outData[i * outWidthStride + j] = std::abs(inData[i * inWidthStride + j]);
+        }
+    }
+    return ppl::common::RC_SUCCESS;
+}
+
+template<>
+::ppl::common::RetCode Abs<float32_t, 1>(
+    int32_t height,
+    int32_t width,
+    int32_t inWidthStride,
+    const float32_t *inData,
+    int32_t outWidthStride,
+    float32_t *outData)
+{
+    return abs_f32(height, width, 1, inWidthStride, inData, outWidthStride, outData);
+}
+
+template<>
+::ppl::common::RetCode Abs<float32_t, 3>(
+    int32_t height,
+    int32_t width,
+    int32_t inWidthStride,
+    const float32_t *inData,
+    int32_t outWidthStride,
+    float32_t *outData)
+{
+    return abs_f32(height, width, 3, inWidthStride, inData, outWidthStride, outData);
+}
+
+template<>
+::ppl::common::RetCode Abs<float32_t, 4>(
+    int32_t height,
+    int32_t width,
+    int32_t inWidthStride,
+    const float32_t *inData,
+    int32_t outWidthStride,
+    float32_t *outData)
+{
+    return abs_f32(height, width, 4, inWidthStride, inData, outWidthStride, outData);
+}
 
 }
 }
