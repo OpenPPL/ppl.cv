d757f647371d749d17575a43263ef294b957aeda
VOIDMalkuth <8085528+VOIDMalkuth@users.noreply.github.com>
2023-06-06 13:01:30 +0000

    [feature][arm] add Transpose Op

---
+++ src/ppl/cv/arm/transpose_benchmark.cpp
@@ -0,0 +1,115 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include "ppl/cv/arm/transpose.h"
+
+#include <chrono>
+
+#include "opencv2/core.hpp"
+#include "benchmark/benchmark.h"
+
+#include "ppl/cv/debug.h"
+#include "utility/infrastructure.hpp"
+
+using namespace ppl::cv::debug;
+
+template <typename T, int channels>
+void BM_Transpose_ppl_aarch64(benchmark::State &state)
+{
+    int width = state.range(0);
+    int height = state.range(1);
+    cv::Mat src = createSourceImage(height, width, CV_MAKETYPE(cv::DataType<T>::depth, channels));
+    cv::Mat dst(width, height, CV_MAKETYPE(cv::DataType<T>::depth, channels));
+
+    int warmup_iters = 5;
+    int perf_iters = 50;
+
+    // Warm up the CPU.
+    for (int i = 0; i < warmup_iters; i++) {
+        ppl::cv::arm::Transpose<T, channels>(
+            src.rows, src.cols, src.step / sizeof(T), (T *)src.data, dst.step / sizeof(T), (T *)dst.data);
+    }
+
+    for (auto _ : state) {
+        auto time_start = std::chrono::high_resolution_clock::now();
+        for (int i = 0; i < perf_iters; i++) {
+            ppl::cv::arm::Transpose<T, channels>(
+                src.rows, src.cols, src.step / sizeof(T), (T *)src.data, dst.step / sizeof(T), (T *)dst.data);
+        }
+        auto time_end = std::chrono::high_resolution_clock::now();
+        auto duration = time_end - time_start;
+        auto overall_time = std::chrono::duration_cast<std::chrono::microseconds>(duration).count();
+        double time = overall_time * 1.0 / perf_iters;
+        state.SetIterationTime(time * 1e-6);
+    }
+    state.SetItemsProcessed(state.iterations() * 1);
+}
+
+#define RUN_PPL_CV_TYPE_FUNCTIONS(type)                                                                          \
+    BENCHMARK_TEMPLATE(BM_Transpose_ppl_aarch64, type, c1)->Args({640, 480})->UseManualTime()->Iterations(10);   \
+    BENCHMARK_TEMPLATE(BM_Transpose_ppl_aarch64, type, c3)->Args({640, 480})->UseManualTime()->Iterations(10);   \
+    BENCHMARK_TEMPLATE(BM_Transpose_ppl_aarch64, type, c4)->Args({640, 480})->UseManualTime()->Iterations(10);   \
+    BENCHMARK_TEMPLATE(BM_Transpose_ppl_aarch64, type, c1)->Args({1920, 1080})->UseManualTime()->Iterations(10); \
+    BENCHMARK_TEMPLATE(BM_Transpose_ppl_aarch64, type, c3)->Args({1920, 1080})->UseManualTime()->Iterations(10); \
+    BENCHMARK_TEMPLATE(BM_Transpose_ppl_aarch64, type, c4)->Args({1920, 1080})->UseManualTime()->Iterations(10);
+
+RUN_PPL_CV_TYPE_FUNCTIONS(float)
+RUN_PPL_CV_TYPE_FUNCTIONS(uint8_t)
+
+#ifdef PPLCV_BENCHMARK_OPENCV
+template <typename T, int32_t channels>
+void BM_Transpose_opencv_aarch64(benchmark::State &state)
+{
+    int32_t width = state.range(0);
+    int32_t height = state.range(1);
+    cv::Mat src = createSourceImage(height, width, CV_MAKETYPE(cv::DataType<T>::depth, channels));
+    cv::Mat dst(width, height, CV_MAKETYPE(cv::DataType<T>::depth, channels));
+
+    int warmup_iters = 5;
+    int perf_iters = 50;
+
+    // Warm up the CPU.
+    for (int i = 0; i < warmup_iters; i++) {
+        cv::transpose(src, dst);
+    }
+
+    for (auto _ : state) {
+        auto time_start = std::chrono::high_resolution_clock::now();
+        for (int i = 0; i < perf_iters; i++) {
+            cv::transpose(src, dst);
+        }
+        auto time_end = std::chrono::high_resolution_clock::now();
+        auto duration = time_end - time_start;
+        auto overall_time = std::chrono::duration_cast<std::chrono::microseconds>(duration).count();
+        double time = overall_time * 1.0 / perf_iters;
+        state.SetIterationTime(time * 1e-6);
+    }
+    state.SetItemsProcessed(state.iterations() * 1);
+}
+
+#define RUN_OPENCV_TYPE_FUNCTIONS(type)                                                                             \
+    BENCHMARK_TEMPLATE(BM_Transpose_opencv_aarch64, type, c1)->Args({640, 480})->UseManualTime()->Iterations(10);   \
+    BENCHMARK_TEMPLATE(BM_Transpose_opencv_aarch64, type, c3)->Args({640, 480})->UseManualTime()->Iterations(10);   \
+    BENCHMARK_TEMPLATE(BM_Transpose_opencv_aarch64, type, c4)->Args({640, 480})->UseManualTime()->Iterations(10);   \
+    BENCHMARK_TEMPLATE(BM_Transpose_opencv_aarch64, type, c1)->Args({1920, 1080})->UseManualTime()->Iterations(10); \
+    BENCHMARK_TEMPLATE(BM_Transpose_opencv_aarch64, type, c3)->Args({1920, 1080})->UseManualTime()->Iterations(10); \
+    BENCHMARK_TEMPLATE(BM_Transpose_opencv_aarch64, type, c4)->Args({1920, 1080})->UseManualTime()->Iterations(10);
+
+RUN_OPENCV_TYPE_FUNCTIONS(float)
+RUN_OPENCV_TYPE_FUNCTIONS(uint8_t)
+
+#endif //! PPLCV_BENCHMARK_OPENCV
---
+++ src/ppl/cv/arm/transpose_unittest.cpp
@@ -0,0 +1,108 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include "ppl/cv/arm/transpose.h"
+
+#include <tuple>
+#include <sstream>
+
+#include "opencv2/core.hpp"
+#include "gtest/gtest.h"
+
+#include "utility/infrastructure.hpp"
+
+using Parameters = std::tuple<cv::Size>;
+inline std::string convertToString(const Parameters& parameters)
+{
+    std::ostringstream formatted;
+
+    cv::Size size = std::get<0>(parameters);
+    formatted << size.width << "x";
+    formatted << size.height;
+
+    return formatted.str();
+}
+
+template <typename T, int channels>
+class PplCvArmTransposeTest : public ::testing::TestWithParam<Parameters> {
+public:
+    PplCvArmTransposeTest()
+    {
+        const Parameters& parameters = GetParam();
+        size = std::get<0>(parameters);
+    }
+
+    ~PplCvArmTransposeTest() {}
+
+    bool apply();
+
+private:
+    cv::Size size;
+};
+
+template <typename T, int channels>
+bool PplCvArmTransposeTest<T, channels>::apply()
+{
+    cv::Mat src = createSourceImage(size.height, size.width, CV_MAKETYPE(cv::DataType<T>::depth, channels));
+    cv::Mat dst(size.width, size.height, CV_MAKETYPE(cv::DataType<T>::depth, channels));
+    cv::Mat cv_dst(size.width, size.height, CV_MAKETYPE(cv::DataType<T>::depth, channels));
+
+    cv::transpose(src, cv_dst);
+
+    ppl::cv::arm::Transpose<T, channels>(
+        src.rows, src.cols, src.step / sizeof(T), (T*)src.data, dst.step / sizeof(T), (T*)dst.data);
+
+    float epsilon;
+    if (sizeof(T) == 1) {
+        epsilon = EPSILON_1F;
+    } else {
+        epsilon = EPSILON_E6;
+    }
+    bool identity = checkMatricesIdentity<T>(cv_dst, dst, epsilon);
+
+    return identity;
+}
+
+#define UNITTEST(T, channels)                                                                       \
+    using PplCvArmTransposeTest_##T##_##channels = PplCvArmTransposeTest<T, channels>;              \
+    TEST_P(PplCvArmTransposeTest_##T##_##channels, Standard)                                        \
+    {                                                                                               \
+        bool identity = this->apply();                                                              \
+        EXPECT_TRUE(identity);                                                                      \
+    }                                                                                               \
+                                                                                                    \
+    INSTANTIATE_TEST_CASE_P(                                                                        \
+        IsEqual,                                                                                    \
+        PplCvArmTransposeTest_##T##_##channels,                                                     \
+        ::testing::Values(cv::Size{321, 240},                                                       \
+                          cv::Size{642, 480},                                                       \
+                          cv::Size{1283, 720},                                                      \
+                          cv::Size{1934, 1080},                                                     \
+                          cv::Size{320, 240},                                                       \
+                          cv::Size{640, 480},                                                       \
+                          cv::Size{1280, 720},                                                      \
+                          cv::Size{1920, 1080}),                                                    \
+        [](const testing::TestParamInfo<PplCvArmTransposeTest_##T##_##channels::ParamType>& info) { \
+            return convertToString(info.param);                                                     \
+        });
+
+UNITTEST(float, 1)
+UNITTEST(float, 3)
+UNITTEST(float, 4)
+UNITTEST(uint8_t, 1)
+UNITTEST(uint8_t, 3)
+UNITTEST(uint8_t, 4)
