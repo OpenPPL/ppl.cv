6be61e1be07565ab97d251405e33059e3915bffe
VOIDMalkuth <8085528+VOIDMalkuth@users.noreply.github.com>
2023-06-07 16:12:50 +0000

    [feature][arm] add BoxFilter Op

---
+++ include/ppl/cv/arm/boxfilter.h
@@ -0,0 +1,96 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#ifndef __ST_HPC_PPL_CV_X86_BOXFILTER_H_
+#define __ST_HPC_PPL_CV_X86_BOXFILTER_H_
+
+#include "ppl/common/retcode.h"
+#include "ppl/cv/types.h"
+
+namespace ppl {
+namespace cv {
+namespace arm {
+
+/**
+* @brief Box Filter,Process boundaries.
+* @tparam T The data type of input image, currently only \a uint8_t(uchar) and \a float are supported.
+* @tparam channels The number of channels of input image, 1, 3 and 4 are supported.
+* @param height            input image's height
+* @param width             input image's width need to be processed
+* @param inWidthStride     input image's width stride, usually it equals to `width * channels`
+* @param inData            input image data
+* @param ksize_x           Filter size, x direction
+* @param ksize_y           Filter size, y direction
+* @param normalize         Whether it needs to be normalized
+* @param outWidthStride    the width stride of output image, usually it equals to `width * channels`
+* @param outData           output image data
+* @param border_type       ways to deal with border. Only BORDER_REFLECT, BORDER_REFLECT_101 or BORDER_DEFAULT are supported now.
+* @warning All input parameters must be valid, or undefined behaviour may occur.
+* @remark The following table show which data type and channels are supported.
+* <table>
+* <tr><th>Data type(T)<th>channels
+* <tr><td>uint8_t(uchar)<td>1
+* <tr><td>uint8_t(uchar)<td>3
+* <tr><td>uint8_t(uchar)<td>4
+* <tr><td>float<td>1
+* <tr><td>float<td>3
+* <tr><td>float<td>4
+* </table>
+* <table>
+* <caption align="left">Requirements</caption>
+* <tr><td>arm platforms supported<td> All 
+* <tr><td>Header files<td> #include &lt;ppl/cv/arm/boxfilter.h&gt;
+* <tr><td>Project<td> ppl.cv
+* @since ppl.cv-v1.0.0
+* ###Example
+* @code{.cpp}
+* #include <ppl/cv/arm/boxfilter.h>
+* int32_t main(int32_t argc, char** argv) {
+*     const int32_t W = 640;
+*     const int32_t H = 480;
+*     const int32_t C = 3;
+*     const int32_t ksize_x = 3;
+*     const int32_t ksize_y = 3;
+*     bool normalize = 0;
+*     float* dev_iImage = (float*)malloc(W * H * C * sizeof(float));
+*     float* dev_oImage = (float*)malloc(W * H * C * sizeof(float));
+*
+*     ppl::cv::arm::BoxFilter<float, 3>(stream, H, W, W * C, dev_iImage, ksize_x, ksize_y, normalize, dev_oImage, ppl::cv::BORDER_DEFAULT);
+*
+*     free(dev_iImage);
+*     free(dev_oImage);
+*     return 0;
+* }
+* @endcode
+***************************************************************************************************/
+template <typename T, int32_t numChannels>
+::ppl::common::RetCode BoxFilter(
+    int32_t height,
+    int32_t width,
+    int32_t inWidthStride,
+    const T* inData,
+    int32_t ksize_x,
+    int32_t ksize_y,
+    bool normalize,
+    int32_t outWidthStride,
+    T* outData,
+    BorderType border_type = BORDER_DEFAULT);
+
+}
+}
+} // namespace ppl::cv::arm
+#endif //! __ST_HPC_PPL_CV_ARM_BOXFILTER_H_
