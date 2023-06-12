69f82ad72a56a6fd479b97c39b8fc0d6d9a04c4e
VOIDMalkuth <8085528+VOIDMalkuth@users.noreply.github.com>
2023-06-12 09:18:17 +0000

    [perf][arm] better merge with prefetch

--- src/ppl/cv/arm/merge_unittest.cpp
+++ src/ppl/cv/arm/merge_unittest.cpp
@@ -111,12 +111,10 @@ bool PplCvArmMergeTest<T, channels>::apply()
                                                                                                                     \
     INSTANTIATE_TEST_CASE_P(IsEqual,                                                                                \
                             PplCvArmMergeTest_##T##_##channels,                                                     \
-                            ::testing::Values(cv::Size{321, 240},                                                   \
-                                              cv::Size{642, 480},                                                   \
-                                              cv::Size{1283, 720},                                                  \
-                                              cv::Size{1934, 1080},                                                 \
-                                              cv::Size{320, 240},                                                   \
-                                              cv::Size{640, 480},                                                   \
+                            ::testing::Values(cv::Size{320, 240},                                                   \
+                                              cv::Size{321, 245},                                                   \
+                                              cv::Size{647, 493},                                                   \
+                                              cv::Size{654, 486},                                                   \
                                               cv::Size{1280, 720},                                                  \
                                               cv::Size{1920, 1080}),                                                \
                             [](const testing::TestParamInfo<PplCvArmMergeTest_##T##_##channels::ParamType>& info) { \
