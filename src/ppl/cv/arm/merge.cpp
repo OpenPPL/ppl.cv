ab6d95acd7c9c51f9465bb11d26481c7d0226556
VOIDMalkuth <8085528+VOIDMalkuth@users.noreply.github.com>
2023-06-12 09:18:17 +0000

    [perf][arm] better merge with prefetch

--- src/ppl/cv/arm/merge.cpp
+++ src/ppl/cv/arm/merge.cpp
@@ -20,25 +20,176 @@
 #include "ppl/common/sys.h"
 #include "common.hpp"
 #include <arm_neon.h>
-
+#include <cstdio>
 namespace ppl::cv::arm {
 
 template <typename T>
-void mergeSOA2AOS(int32_t numChannels,
-                  int32_t height,
-                  int32_t width,
-                  int32_t inWidthStride,
-                  const T** in,
-                  int32_t outWidthStride,
-                  T* out)
+void mergeSOA2AOS_3Channels(int32_t height,
+                            int32_t width,
+                            int32_t inWidthStride,
+                            const T** in,
+                            int32_t outWidthStride,
+                            T* out);
+
+template <>
+void mergeSOA2AOS_3Channels(int32_t height,
+                            int32_t width,
+                            int32_t inWidthStride,
+                            const uint8_t** in,
+                            int32_t outWidthStride,
+                            uint8_t* out)
 {
+    const uint8_t* src0 = in[0];
+    const uint8_t* src1 = in[1];
+    const uint8_t* src2 = in[2];
+    uint8_t* out0 = out;
     for (int32_t h = 0; h < height; h++) {
-        for (int32_t w = 0; w < width; w++) {
-            int32_t inIndex = h * inWidthStride + w;
-            int32_t outIndex = h * outWidthStride + w * numChannels;
-            for (int32_t c = 0; c < numChannels; c++) {
-                out[outIndex + c] = in[c][inIndex];
-            }
+        const uint8_t* src0_row = src0 + h * inWidthStride;
+        const uint8_t* src1_row = src1 + h * inWidthStride;
+        const uint8_t* src2_row = src2 + h * inWidthStride;
+        uint8_t* out0_row = out0 + h * outWidthStride;
+        int32_t w = 0;
+        for (; w <= width - 16; w += 16) {
+            prefetch(src0_row + w);
+            prefetch(src1_row + w);
+            prefetch(src2_row + w);
+            uint8x16x3_t vData;
+            vData.val[0] = vld1q_u8(src0_row + w);
+            vData.val[1] = vld1q_u8(src1_row + w);
+            vData.val[2] = vld1q_u8(src2_row + w);
+            vst3q_u8(out0_row + w * 3, vData);
+        }
+        for (; w < width; w++) {
+            out0_row[w * 3 + 0] = src0_row[w];
+            out0_row[w * 3 + 1] = src1_row[w];
+            out0_row[w * 3 + 2] = src2_row[w];
+        }
+    }
+}
+
+template <>
+void mergeSOA2AOS_3Channels(int32_t height,
+                            int32_t width,
+                            int32_t inWidthStride,
+                            const float** in,
+                            int32_t outWidthStride,
+                            float* out)
+{
+    const float* src0 = in[0];
+    const float* src1 = in[1];
+    const float* src2 = in[2];
+    float* out0 = out;
+    for (int32_t h = 0; h < height; h++) {
+        const float* src0_row = src0 + h * inWidthStride;
+        const float* src1_row = src1 + h * inWidthStride;
+        const float* src2_row = src2 + h * inWidthStride;
+        float* out0_row = out0 + h * outWidthStride;
+        int32_t w = 0;
+        for (; w <= width - 4; w += 4) {
+            prefetch(src0_row + w);
+            prefetch(src1_row + w);
+            prefetch(src2_row + w);
+            float32x4x3_t vData;
+            vData.val[0] = vld1q_f32(src0_row + w);
+            vData.val[1] = vld1q_f32(src1_row + w);
+            vData.val[2] = vld1q_f32(src2_row + w);
+            vst3q_f32(out0_row + w * 3, vData);
+        }
+
+        for (; w < width; w++) {
+            out0_row[w * 3 + 0] = src0_row[w];
+            out0_row[w * 3 + 1] = src1_row[w];
+            out0_row[w * 3 + 2] = src2_row[w];
+        }
+    }
+}
+
+template <typename T>
+void mergeSOA2AOS_4Channels(int32_t height,
+                            int32_t width,
+                            int32_t inWidthStride,
+                            const T** in,
+                            int32_t outWidthStride,
+                            T* out);
+
+template <>
+void mergeSOA2AOS_4Channels(int32_t height,
+                            int32_t width,
+                            int32_t inWidthStride,
+                            const uint8_t** in,
+                            int32_t outWidthStride,
+                            uint8_t* out)
+{
+    const uint8_t* src0 = in[0];
+    const uint8_t* src1 = in[1];
+    const uint8_t* src2 = in[2];
+    const uint8_t* src3 = in[3];
+    uint8_t* out0 = out;
+    for (int32_t h = 0; h < height; h++) {
+        const uint8_t* src0_row = src0 + h * inWidthStride;
+        const uint8_t* src1_row = src1 + h * inWidthStride;
+        const uint8_t* src2_row = src2 + h * inWidthStride;
+        const uint8_t* src3_row = src3 + h * inWidthStride;
+        uint8_t* out0_row = out0 + h * outWidthStride;
+        int32_t w = 0;
+        for (; w <= width - 16; w += 16) {
+            prefetch(src0_row + w);
+            prefetch(src1_row + w);
+            prefetch(src2_row + w);
+            prefetch(src3_row + w);
+            uint8x16x4_t vData;
+            vData.val[0] = vld1q_u8(src0_row + w);
+            vData.val[1] = vld1q_u8(src1_row + w);
+            vData.val[2] = vld1q_u8(src2_row + w);
+            vData.val[3] = vld1q_u8(src3_row + w);
+            vst4q_u8(out0_row + w * 4, vData);
+        }
+        for (; w < width; w++) {
+            out0_row[w * 4 + 0] = src0_row[w];
+            out0_row[w * 4 + 1] = src1_row[w];
+            out0_row[w * 4 + 2] = src2_row[w];
+            out0_row[w * 4 + 3] = src3_row[w];
+        }
+    }
+}
+
+template <>
+void mergeSOA2AOS_4Channels(int32_t height,
+                            int32_t width,
+                            int32_t inWidthStride,
+                            const float** in,
+                            int32_t outWidthStride,
+                            float* out)
+{
+    const float* src0 = in[0];
+    const float* src1 = in[1];
+    const float* src2 = in[2];
+    const float* src3 = in[3];
+    float* out0 = out;
+    for (int32_t h = 0; h < height; h++) {
+        const float* src0_row = src0 + h * inWidthStride;
+        const float* src1_row = src1 + h * inWidthStride;
+        const float* src2_row = src2 + h * inWidthStride;
+        const float* src3_row = src3 + h * inWidthStride;
+        float* out0_row = out0 + h * outWidthStride;
+        int32_t w = 0;
+        for (; w <= width - 4; w += 4) {
+            prefetch(src0_row + w);
+            prefetch(src1_row + w);
+            prefetch(src2_row + w);
+            prefetch(src3_row + w);
+            float32x4x4_t vData;
+            vData.val[0] = vld1q_f32(src0_row + w);
+            vData.val[1] = vld1q_f32(src1_row + w);
+            vData.val[2] = vld1q_f32(src2_row + w);
+            vData.val[3] = vld1q_f32(src3_row + w);
+            vst4q_f32(out0_row + w * 4, vData);
+        }
+        for (; w < width; w++) {
+            out0_row[w * 4 + 0] = src0_row[w];
+            out0_row[w * 4 + 1] = src1_row[w];
+            out0_row[w * 4 + 2] = src2_row[w];
+            out0_row[w * 4 + 3] = src3_row[w];
         }
     }
 }
@@ -59,8 +210,16 @@ template <typename T>
     if (width <= 0 || height <= 0 || inWidthStride < width || outWidthStride < width) {
         return ppl::common::RC_INVALID_VALUE;
     }
+
+    if (inWidthStride == width && outWidthStride == width * 3) {
+        width *= height;
+        inWidthStride *= height;
+        outWidthStride *= height;
+        height = 1;
+    }
+
     const T* inData[3] = {inDataC0, inDataC1, inDataC2};
-    mergeSOA2AOS<T>(3, height, width, inWidthStride, inData, outWidthStride, outData);
+    mergeSOA2AOS_3Channels<T>(height, width, inWidthStride, inData, outWidthStride, outData);
     return ppl::common::RC_SUCCESS;
 }
 
@@ -82,8 +241,16 @@ template <typename T>
     if (width <= 0 || height <= 0 || inWidthStride < width || outWidthStride < width) {
         return ppl::common::RC_INVALID_VALUE;
     }
+
+    if (inWidthStride == width && outWidthStride == width * 4) {
+        width *= height;
+        inWidthStride *= height;
+        outWidthStride *= height;
+        height = 1;
+    }
+
     const T* inData[4] = {inDataC0, inDataC1, inDataC2, inDataC3};
-    mergeSOA2AOS<T>(4, height, width, inWidthStride, inData, outWidthStride, outData);
+    mergeSOA2AOS_4Channels<T>(height, width, inWidthStride, inData, outWidthStride, outData);
     return ppl::common::RC_SUCCESS;
 }
 
