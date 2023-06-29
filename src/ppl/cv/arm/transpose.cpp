b825ffb9e60322d15a91734103d4f4c6cf4a2813
VOIDMalkuth <8085528+VOIDMalkuth@users.noreply.github.com>
2023-06-29 13:47:25 +0000

    [misc][arm] place commonly used neon operations into intrinutils_neon.cpp

--- src/ppl/cv/arm/transpose.cpp
+++ src/ppl/cv/arm/transpose.cpp
@@ -19,104 +19,11 @@
 #include "ppl/cv/types.h"
 #include "ppl/common/sys.h"
 #include "common.hpp"
+#include "intrinutils_neon.hpp"
+
 #include <arm_neon.h>
 
 namespace ppl::cv::arm {
-
-inline void neon_transpose_f32_4x4(float32x4_t &va, float32x4_t &vb, float32x4_t &vc, float32x4_t &vd)
-{
-    float32x4_t v02_0 = vzip1q_f32(va, vc);
-    float32x4_t v13_0 = vzip1q_f32(vb, vd);
-    float32x4_t v02_2 = vzip2q_f32(va, vc);
-    float32x4_t v13_2 = vzip2q_f32(vb, vd);
-    va = vzip1q_f32(v02_0, v13_0);
-    vb = vzip2q_f32(v02_0, v13_0);
-    vc = vzip1q_f32(v02_2, v13_2);
-    vd = vzip2q_f32(v02_2, v13_2);
-}
-
-inline void neon_transpose_u32_4x4(uint32x4_t &va, uint32x4_t &vb, uint32x4_t &vc, uint32x4_t &vd)
-{
-    uint32x4_t v02_0 = vzip1q_u32(va, vc);
-    uint32x4_t v13_0 = vzip1q_u32(vb, vd);
-    uint32x4_t v02_2 = vzip2q_u32(va, vc);
-    uint32x4_t v13_2 = vzip2q_u32(vb, vd);
-    va = vzip1q_u32(v02_0, v13_0);
-    vb = vzip2q_u32(v02_0, v13_0);
-    vc = vzip1q_u32(v02_2, v13_2);
-    vd = vzip2q_u32(v02_2, v13_2);
-}
-
-inline void neon_transpose_u8_8x8(uint8x8_t &va,
-                                  uint8x8_t &vb,
-                                  uint8x8_t &vc,
-                                  uint8x8_t &vd,
-                                  uint8x8_t &ve,
-                                  uint8x8_t &vf,
-                                  uint8x8_t &vg,
-                                  uint8x8_t &vh)
-{
-    uint8x8_t v04_0 = vzip1_u8(va, ve);
-    uint8x8_t v15_0 = vzip1_u8(vb, vf);
-    uint8x8_t v26_0 = vzip1_u8(vc, vg);
-    uint8x8_t v37_0 = vzip1_u8(vd, vh);
-    uint8x8_t v04_4 = vzip2_u8(va, ve);
-    uint8x8_t v15_4 = vzip2_u8(vb, vf);
-    uint8x8_t v26_4 = vzip2_u8(vc, vg);
-    uint8x8_t v37_4 = vzip2_u8(vd, vh);
-
-    uint8x8_t v0246_0 = vzip1_u8(v04_0, v26_0);
-    uint8x8_t v1357_0 = vzip1_u8(v15_0, v37_0);
-    uint8x8_t v0246_2 = vzip2_u8(v04_0, v26_0);
-    uint8x8_t v1357_2 = vzip2_u8(v15_0, v37_0);
-    uint8x8_t v0246_4 = vzip1_u8(v04_4, v26_4);
-    uint8x8_t v1357_4 = vzip1_u8(v15_4, v37_4);
-    uint8x8_t v0246_6 = vzip2_u8(v04_4, v26_4);
-    uint8x8_t v1357_6 = vzip2_u8(v15_4, v37_4);
-
-    va = vzip1_u8(v0246_0, v1357_0);
-    vb = vzip2_u8(v0246_0, v1357_0);
-    vc = vzip1_u8(v0246_2, v1357_2);
-    vd = vzip2_u8(v0246_2, v1357_2);
-    ve = vzip1_u8(v0246_4, v1357_4);
-    vf = vzip2_u8(v0246_4, v1357_4);
-    vg = vzip1_u8(v0246_6, v1357_6);
-    vh = vzip2_u8(v0246_6, v1357_6);
-}
-
-inline void neon_transpose_u8_8x8_new_device(uint8x8_t &va,
-                                  uint8x8_t &vb,
-                                  uint8x8_t &vc,
-                                  uint8x8_t &vd,
-                                  uint8x8_t &ve,
-                                  uint8x8_t &vf,
-                                  uint8x8_t &vg,
-                                  uint8x8_t &vh)
-{
-    uint8x16_t v04 = vzip1q_u8(vcombine_u8(va, va), vcombine_u8(ve, ve));
-    uint8x16_t v15 = vzip1q_u8(vcombine_u8(vb, vb), vcombine_u8(vf, vf));
-    uint8x16_t v26 = vzip1q_u8(vcombine_u8(vc, vc), vcombine_u8(vg, vg));
-    uint8x16_t v37 = vzip1q_u8(vcombine_u8(vd, vd), vcombine_u8(vh, vh));
-
-    uint8x16_t v0246_0 = vzip1q_u8(v04, v26);
-    uint8x16_t v1357_0 = vzip1q_u8(v15, v37);
-    uint8x16_t v0246_4 = vzip2q_u8(v04, v26);
-    uint8x16_t v1357_4 = vzip2q_u8(v15, v37);
-
-    uint8x16_t vab = vzip1q_u8(v0246_0, v1357_0);
-    uint8x16_t vcd = vzip2q_u8(v0246_0, v1357_0);
-    uint8x16_t vef = vzip1q_u8(v0246_4, v1357_4);
-    uint8x16_t vgh = vzip2q_u8(v0246_4, v1357_4);
-
-    va = vget_low_u8(vab);
-    vb = vget_high_u8(vab);
-    vc = vget_low_u8(vcd);
-    vd = vget_high_u8(vcd);
-    ve = vget_low_u8(vef);
-    vf = vget_high_u8(vef);
-    vg = vget_low_u8(vgh);
-    vh = vget_high_u8(vgh);
-}
 
 // Applies to perfect square blocks only
 template <typename T, int CHANNELS, int OUT_BLOCKDIM_R, int OUT_BLOCKDIM_C>
