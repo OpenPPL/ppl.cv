664dbded9c8406c7cb1214adb9e23f9402a9700c
VOIDMalkuth <8085528+VOIDMalkuth@users.noreply.github.com>
2023-06-29 16:31:32 +0000

    [perf][arm] optimize 180 degree rotation

--- src/ppl/cv/arm/rotate.cpp
+++ src/ppl/cv/arm/rotate.cpp
@@ -19,6 +19,7 @@
 #include "ppl/cv/types.h"
 #include "ppl/common/sys.h"
 #include "common.hpp"
+#include "intrinutils_neon.hpp"
 #include <arm_neon.h>
 
 namespace ppl::cv::arm {
@@ -80,6 +81,160 @@ void imgRotate180degree(int32_t inHeight,
             for (int32_t c = 0; c < nc; c++) {
                 outData[i * outWidthStride + j * nc + c] =
                     inData[(inHeight - i - 1) * inWidthStride + (inWidth - j - 1) * nc + c];
+            }
+        }
+    }
+}
+
+template <>
+void imgRotate180degree<float, 1>(int32_t inHeight,
+                                  int32_t inWidth,
+                                  int32_t inWidthStride,
+                                  const float* inData,
+                                  int32_t outHeight,
+                                  int32_t outWidth,
+                                  int32_t outWidthStride,
+                                  float* outData)
+{
+    constexpr int nc = 1;
+    for (int32_t i = 0; i < inHeight; i++) {
+        const float* in = inData + i * inWidthStride;
+        float* out = outData + (outHeight - i - 1) * outWidthStride;
+        int32_t j = 0;
+        for (; j <= inWidth - 8; j += 8) {
+            prefetch(in + j * nc);
+            float32x2_t vdata0 = vld1_f32(in + (j + 0) * nc);
+            float32x2_t vdata1 = vld1_f32(in + (j + 2) * nc);
+            float32x2_t vdata2 = vld1_f32(in + (j + 4) * nc);
+            float32x2_t vdata3 = vld1_f32(in + (j + 6) * nc);
+
+            float32x2_t vout3 = neon_reverse_f32x2(vdata0);
+            float32x2_t vout2 = neon_reverse_f32x2(vdata1);
+            float32x2_t vout1 = neon_reverse_f32x2(vdata2);
+            float32x2_t vout0 = neon_reverse_f32x2(vdata3);
+
+            vst1_f32(out + (outWidth - j - 1 - 7) * nc, vout0);
+            vst1_f32(out + (outWidth - j - 1 - 5) * nc, vout1);
+            vst1_f32(out + (outWidth - j - 1 - 3) * nc, vout2);
+            vst1_f32(out + (outWidth - j - 1 - 1) * nc, vout3);
+        }
+        for (; j <= inWidth - 2; j += 2) {
+            float32x2_t vdata0 = vld1_f32(in + (j + 0) * nc);
+            float32x2_t vout0 = neon_reverse_f32x2(vdata0);
+            vst1_f32(out + (outWidth - j - 1 - 1) * nc, vout0);
+        }
+        for (; j < inWidth; j++) {
+            for (int32_t c = 0; c < nc; c++) {
+                outData[(outHeight - i - 1) * outWidthStride + (outWidth - j - 1) * nc + c] =
+                    inData[i * inWidthStride + j * nc + c];
+            }
+        }
+    }
+}
+
+template <>
+void imgRotate180degree<uint8_t, 1>(int32_t inHeight,
+                                    int32_t inWidth,
+                                    int32_t inWidthStride,
+                                    const uint8_t* inData,
+                                    int32_t outHeight,
+                                    int32_t outWidth,
+                                    int32_t outWidthStride,
+                                    uint8_t* outData)
+{
+    constexpr int nc = 1;
+    for (int32_t i = 0; i < inHeight; i++) {
+        const uint8_t* in = inData + i * inWidthStride;
+        uint8_t* out = outData + (outHeight - i - 1) * outWidthStride;
+        int32_t j = 0;
+        for (; j <= inWidth - 32; j += 32) {
+            prefetch(in + j * nc);
+            prefetch(in + (j + 16) * nc);
+            uint8x8_t vdata0 = vld1_u8(in + (j + 0) * nc);
+            uint8x8_t vdata1 = vld1_u8(in + (j + 8) * nc);
+            uint8x8_t vdata2 = vld1_u8(in + (j + 16) * nc);
+            uint8x8_t vdata3 = vld1_u8(in + (j + 24) * nc);
+
+            uint8x8_t vout3 = neon_reverse_u8x8(vdata0);
+            uint8x8_t vout2 = neon_reverse_u8x8(vdata1);
+            uint8x8_t vout1 = neon_reverse_u8x8(vdata2);
+            uint8x8_t vout0 = neon_reverse_u8x8(vdata3);
+
+            vst1_u8(out + (outWidth - j - 1 - 31) * nc, vout0);
+            vst1_u8(out + (outWidth - j - 1 - 23) * nc, vout1);
+            vst1_u8(out + (outWidth - j - 1 - 15) * nc, vout2);
+            vst1_u8(out + (outWidth - j - 1 - 7) * nc, vout3);
+        }
+        for (; j <= inWidth - 8; j += 8) {
+            uint8x8_t vdata0 = vld1_u8(in + (j + 0) * nc);
+            uint8x8_t vout0 = neon_reverse_u8x8(vdata0);
+            vst1_u8(out + (outWidth - j - 1 - 7) * nc, vout0);
+        }
+        for (; j < inWidth; j++) {
+            for (int32_t c = 0; c < nc; c++) {
+                outData[(outHeight - i - 1) * outWidthStride + (outWidth - j - 1) * nc + c] =
+                    inData[i * inWidthStride + j * nc + c];
+            }
+        }
+    }
+}
+
+template <>
+void imgRotate180degree<uint8_t, 3>(int32_t inHeight,
+                                    int32_t inWidth,
+                                    int32_t inWidthStride,
+                                    const uint8_t* inData,
+                                    int32_t outHeight,
+                                    int32_t outWidth,
+                                    int32_t outWidthStride,
+                                    uint8_t* outData)
+{
+    constexpr int nc = 3;
+    for (int32_t i = 0; i < inHeight; i++) {
+        const uint8_t* in = inData + i * inWidthStride;
+        uint8_t* out = outData + (outHeight - i - 1) * outWidthStride;
+        int32_t j = 0;
+        for (; j <= inWidth - 32; j += 32) {
+            prefetch(in + j * nc);
+            prefetch(in + (j + 8) * nc);
+            prefetch(in + (j + 16) * nc);
+            prefetch(in + (j + 24) * nc);
+            uint8x8x3_t vdata0 = vld3_u8(in + (j + 0) * nc);
+            uint8x8x3_t vdata1 = vld3_u8(in + (j + 8) * nc);
+            uint8x8x3_t vdata2 = vld3_u8(in + (j + 16) * nc);
+            uint8x8x3_t vdata3 = vld3_u8(in + (j + 24) * nc);
+
+            uint8x8x3_t vout3, vout2, vout1, vout0;
+            vout3.val[0] = neon_reverse_u8x8(vdata0.val[0]);
+            vout3.val[1] = neon_reverse_u8x8(vdata0.val[1]);
+            vout3.val[2] = neon_reverse_u8x8(vdata0.val[2]);
+            vout2.val[0] = neon_reverse_u8x8(vdata1.val[0]);
+            vout2.val[1] = neon_reverse_u8x8(vdata1.val[1]);
+            vout2.val[2] = neon_reverse_u8x8(vdata1.val[2]);
+            vout1.val[0] = neon_reverse_u8x8(vdata2.val[0]);
+            vout1.val[1] = neon_reverse_u8x8(vdata2.val[1]);
+            vout1.val[2] = neon_reverse_u8x8(vdata2.val[2]);
+            vout0.val[0] = neon_reverse_u8x8(vdata3.val[0]);
+            vout0.val[1] = neon_reverse_u8x8(vdata3.val[1]);
+            vout0.val[2] = neon_reverse_u8x8(vdata3.val[2]);
+
+            vst3_u8(out + (outWidth - j - 1 - 31) * nc, vout0);
+            vst3_u8(out + (outWidth - j - 1 - 23) * nc, vout1);
+            vst3_u8(out + (outWidth - j - 1 - 15) * nc, vout2);
+            vst3_u8(out + (outWidth - j - 1 - 7) * nc, vout3);
+        }
+        for (; j <= inWidth - 8; j += 8) {
+            uint8x8x3_t vdata0 = vld3_u8(in + (j + 0) * nc);
+            uint8x8x3_t vout0;
+            vout0.val[0] = neon_reverse_u8x8(vdata0.val[0]);
+            vout0.val[1] = neon_reverse_u8x8(vdata0.val[1]);
+            vout0.val[2] = neon_reverse_u8x8(vdata0.val[2]);
+            vst3_u8(out + (outWidth - j - 1 - 7) * nc, vout0);
+        }
+        for (; j < inWidth; j++) {
+            for (int32_t c = 0; c < nc; c++) {
+                outData[(outHeight - i - 1) * outWidthStride + (outWidth - j - 1) * nc + c] =
+                    inData[i * inWidthStride + j * nc + c];
             }
         }
     }
